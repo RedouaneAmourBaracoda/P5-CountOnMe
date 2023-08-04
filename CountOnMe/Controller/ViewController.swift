989323135951bfc4174eb60be28daf555b5a0283
RedouaneAmourBaracoda <redouane.amour@baracoda.com>
2023-08-04 16:36:21 +0200

    layout + assets added.

---
+++ CountOnMe/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalc
+//
+//  Created by Vincent Saluzzo on 29/03/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    // MARK: - View Life cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
